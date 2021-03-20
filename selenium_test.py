e9a9866a613faf045a6d0a84ec4d876214b1303c
raffaele <lele.raffa97@gmail.com>
2021-03-20 19:30:59 +0000

    selenium

---
+++ selenium_test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from random import randint
+
+browser = webdriver.Chrome()
